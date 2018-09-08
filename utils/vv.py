487e212cb3e9cec74c37bbed5948630c37c9bd8c
andrewschultz <schultz.andrew@sbcglobal.net>
2018-09-08 00:42:23 -0500

    vv.py now capitalizes words and gives special note to Best Guesses

--- utils/vv.py
+++ utils/vv.py
@@ -3,19 +3,45 @@
 # at first for 26 letters, but later, sounds
 #
 
+from collections import defaultdict
 from string import ascii_lowercase
 import sys
 
+verbose = False
 standard_input = False
 
 word_ary = []
 default_array = [ 'vast', 'void' ]
 
+word_dict = defaultdict(lambda: defaultdict(bool))
+
 ##############start functions
 
+def word_upper(q):
+    if q in word_dict[len(q)].keys(): return (q.upper(), True)
+    return (q.lower(), False)
+
 def write_all_26(a, b):
+    read_words_of_length(a)
+    read_words_of_length(b)
     for x in ascii_lowercase:
-        print(x + a[1:], x + b[1:])
+        if x == a[0]: continue
+        q = word_upper(x + a[1:])
+        r = word_upper(x + b[1:])
+        xtra_string = "" if q[1] + r[1] < 2 else "<- DOUBLE MATCH"
+        print(q[0], r[0], xtra_string)
+
+def read_words_of_length(a):
+    la = len(a)
+    if la in word_dict.keys():
+        if verbose:
+            print("Already read words of length", la, "so returning.")
+        return
+    my_file = "c:/writing/dict/words-{:d}.txt".format(la)
+    with open(my_file) as f:
+        for line in f:
+            word_dict[la][line.lower().strip()] = True
+    print("Read in", len(word_dict[la].keys()), "words", "for", la)
 
 ##############end functions
 
