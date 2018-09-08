dcda55b6aa814971687abf6dce9666a96ab1a1bd
andrewschultz <schultz.andrew@sbcglobal.net>
2018-09-08 00:28:08 -0500

    vv.py has STDIN now

--- utils/vv.py
+++ utils/vv.py
@@ -1,12 +1,41 @@
+# vv.py: lets the user see all the possible string replacements
+#
+# at first for 26 letters, but later, sounds
+#
+
 from string import ascii_lowercase
 import sys
 
-if len(sys.argv) > 2:
-	w1 = sys.argv[1].lower()
-	w2 = sys.argv[2].lower()
-else:
-	w1 = 'vast'
-	w2 = 'void'
+standard_input = False
 
-for x in ascii_lowercase:
-	print(x + w1[1:], x + w2[1:])
+word_ary = []
+default_array = [ 'vast', 'void' ]
+
+##############start functions
+
+def write_all_26(a, b):
+    for x in ascii_lowercase:
+        print(x + a[1:], x + b[1:])
+
+##############end functions
+
+count = 1
+
+while count < len(sys.argv):
+    arg = sys.argv[count].lower()
+    if arg == 'i': standard_input = True
+    else: word_ary.append(arg)
+    count += 1
+
+if standard_input:
+    keep_going = True
+    while keep_going:
+        x = input(">>")
+        si = x.lower().strip().split(" ")
+        if x == 'q': break
+        if not x.strip(): break
+        write_all_26(si[0], si[1])
+else:
+    for i in range(0, len(word_ary) // 2):
+        write_all_26(word_ary[i*2], word_ary[i*2+1])
+    if len(word_ary) % 2: print("Warning odd number of characters in word array. Last is", word_ary[len(word_ary)-1])
