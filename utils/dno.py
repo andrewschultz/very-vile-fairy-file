937ba98e717e5103cd9106dab338b356d0c0b51b
andrewschultz <schultz.andrew@sbcglobal.net>
2018-11-05 23:19:49 -0600

    dno.py now tracks multiple dupes without flooding screen

--- utils/dno.py
+++ utils/dno.py
@@ -9,6 +9,8 @@ import os
 import sys
 
 note_dict = defaultdict(int)
+already_done = defaultdict(int)
+source_dupes = defaultdict(list)
 dupes = 0
 internal_dupes = 0
 
@@ -23,8 +25,10 @@ def read_notes_file(j):
             lla = re.split(" *\/ *", ll)
             for q in lla:
                 if q and q in note_dict.keys():
-                    internal_dupes += 1
-                    print(q, "already in note_dict.keys at line", note_dict[q], "duplicated at line", line_count)
+                    if q in already_done.keys():
+                        internal_dupes += 1
+                        print(q, "already in note_dict.keys at line", note_dict[q], "duplicated at line", line_count)
+                    already_done[q] += 1
                     continue
                 else: note_dict[q] = line_count
     print("END INTERNAL CHECK FOR", j)
@@ -40,6 +44,7 @@ def read_source_files(j):
                 for q in note_dict.keys():
                     if q in line.lower():
                         print("DUPLICATE <<{:s} line {:d}>>".format(q, note_dict[q]), "From <<{:s} line {:d}>> =\n    {:s}".format(os.path.basename(x), line_count, line.strip()))
+                        source_dupes[q].append("{:s} L{:d}".format(os.path.basename(x), line_count))
                         dupes += 1
 
 read_notes_file("very-vile-fairy-file")
@@ -47,3 +52,5 @@ read_source_files("very-vile-fairy-file")
 
 print("Total duplicates:", dupes)
 print("Total internal duplicates:", internal_dupes)
+
+for j in sorted(source_dupes.keys()): print(j, "appears", len(source_dupes[j]), "extra times in the source:", '/'.join(source_dupes[j]))
