d7da3b2ee45bea7cd05d7465b23d029cc46e1035
andrewschultz <schultz.andrew@sbcglobal.net>
2019-08-19 23:24:25 -0500

    vc/vr rule organization check

--- utils/vc.py
+++ utils/vc.py
@@ -6,13 +6,31 @@ import re
 import sys
 import pyperclip
 import mytools as mt
+from i7 import is_outline_start
+from collections import defaultdict
+
+ignore_dict = defaultdict(bool)
+ignore_array = [ "this is the verb-checker rule", "carry out blowbying" ]
+
+for x in ignore_array: ignore_dict[x] = True
 
 def get_ups():
     new_rule = "<NO RULE YET>"
     rule_line = 0
     count = 0
+    in_vr_rules = False
+    out_of_place = []
     with open("story.ni") as file:
         for (line_count, line) in enumerate(file, 1):
+            if in_vr_rules and is_outline_start(line):
+                in_vr_rules = False
+                in_vr_end = line_count
+                continue
+            if line.startswith("section vc vr rules"):
+                in_vr_rules = True
+                in_vr_start = line_count
+            if not in_vr_rules and (line.startswith("this is the vr-") or line.startswith("this is the vc-")):
+                out_of_place.append("OUT OF PLACE rule {0} at line {1}".format(line.strip(), line_count))
             if not line.startswith("\t"):
                 if not line.startswith("["):
                     new_rule = line.strip()
@@ -21,18 +39,27 @@ def get_ups():
             else:
                 ls = line.strip();
                 if ls.startswith("up-"):
-                    if new_rule is "this is the verb-checker rule": continue
+                    if new_rule in ignore_list: continue
                     count += 1
                     print("RULE", count, rule_line, new_rule)
                     print("    UP/DOWN", line_count, ls)
-                    mt.add_postopen_file_line("story.ni", line_count)
+                    if "vc" in new_rule:
+                        print("UH OH NEW RULE NEEDS ERASING.")
+                        mt.add_postopen_file_line("story.ni", line_count, True)
+                    else:
+                        mt.add_postopen_file_line("story.ni", line_count)
+    if len(out_of_place):
+        print("vc vr rules between", in_vr_start, "and", in_vr_end)
+        print("\n".join(out_of_place))
+    else:
+        print("Go me, or former me! All the vc/vr rules are ORGANIZED.")
     mt.postopen_files()
 
 myary = []
 
 lsa = len(sys.argv)
 
-if lsa > 1 and sys.argv[1] == 'up':
+if lsa > 1 and sys.argv[1] == 'u':
     get_ups()
     exit()
 
@@ -44,7 +71,7 @@ elif lsa == 2:
         myary = re.split("[^a-zA-Z]", sys.argv[1])
 
 if len(myary) != 2:
-    print("Need 2 args, or 1 arg with a separator. Or use up to track the remaining stuff to delete.")
+    print("Need 2 args, or 1 arg with a separator. Or use u to track the remaining stuff to delete.")
     exit()
 
 w1 = myary[0]
