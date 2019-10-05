95ebd5912e55e2691bba14f0527f59e76ac14511
andrewschultz <schultz.andrew@sbcglobal.net>
2019-10-05 01:33:49 -0500

    mch now prints out desired text

--- utils/mch.py
+++ utils/mch.py
@@ -3,6 +3,7 @@
 #
 # mistake vs test script checker
 
+import mytools as mt
 import sys
 from collections import defaultdict
 import re
@@ -25,7 +26,7 @@ def first_slash(x):
     return re.sub("[\|/][a-zA-Z]+", "", x)
 
 def noquo_single(top):
-    top = re.sub("\" *\[.*?\]", "", top)
+    top = i7.rmbrax(top, replace_string = "/")
     if top.startswith('"'): top = top[1:]
     return re.sub("\".*", "", top)
     
@@ -38,6 +39,7 @@ def noquo(topic):
 with open(mist_file) as file:
     for (line_count, line) in enumerate(file, 1):
         if "\t" not in line: continue
+        if "\t\t" in line: sys.exit("Double tabs line {}".format(line_count))
         ary = re.split("\t", line.strip())
         if '"' not in ary[0] or len(ary) == 1: continue
         try:
@@ -54,6 +56,8 @@ with open(mist_file) as file:
 
 okay_next_dup = False
 
+in_mistakes = False
+
 with open("rbr-vvff-thru.txt") as file:
     for (line_count, line) in enumerate(file, 1):
         if okay_next_dup:
@@ -64,10 +68,13 @@ with open("rbr-vvff-thru.txt") as file:
         if not line.strip():
             in_mistakes = False
             continue
+        if not in_mistakes:
+            if 'Learner needle' in line:
+                print("Maybe errant needle message line {}.".format(line_count))
+            continue
         if not line.startswith(">"): continue
         line_cmd = re.sub("^> *", "", line.lower().strip())
         if line_cmd == 'undo': continue
-        if not in_mistakes: continue
         if line_cmd not in need_mistake_test:
             print("Erroneous mistake command line {}: {}.".format(line_count, line_cmd))
         elif line_cmd in got_mistake_test:
@@ -78,9 +85,12 @@ with open("rbr-vvff-thru.txt") as file:
             got_leet_check[line_cmd] = True
         got_mistake_test[line_cmd] = True
 
+err_found = 0
+
 y = [x for x in need_mistake_test if x not in got_mistake_test]
 
 if len(y):
+    err_found += len(y)
     count = 0
     print(len(y), "mistake checks needed.")
     for q in sorted(y, key=lambda x:my_line[x]):
@@ -94,12 +104,15 @@ else: print("No mistake checks needed.")
 
 y = [x for x in got_mistake_test if x not in need_mistake_test]
 
-if len(y): print("Extraneous mistake checks({}): {}".format(len(y), ', '.join(sorted(y, key=lambda x:my_line[x]))))
+if len(y):
+    err_found += len(y)
+    print("Extraneous mistake checks({}): {}".format(len(y), ', '.join(sorted(y, key=lambda x:my_line[x]))))
 else: print("No extraneous mistake checks.")
 
 y = [x for x in need_leet_check if need_leet_check[x] and x not in got_leet_check]
 
 if len(y):
+    err_found += len(y)
     count = 0
     print(len(y), "leet checks needed.")
     for q in sorted(y, key=lambda x:my_line[x]):
@@ -108,13 +121,18 @@ if len(y):
             print("Went over max.")
             break;
         print(">", q)
-        tn = re.sub("^[^a-zA-Z]", "!", text_needed[q])
+        tn = re.sub("^([^a-zA-Z])?", "!", text_needed[q])
         print(tn)
 else: print("No leet checks needed.")
 
 y = [x for x in got_mistake_test if x not in need_mistake_test or need_mistake_test[x] == False]
 
-if len(y): print("Extraneous leet checks({}): {}".format(len(y), ', '.join(sorted(y, key=lambda x:my_line[x]))))
+if len(y):
+    err_found += len(y)
+    print("Extraneous leet checks({}): {}".format(len(y), ', '.join(sorted(y, key=lambda x:my_line[x]))))
 else: print("No extraneous leet checks.")
 
-print("Ended checks.")
+if err_found:
+    print(err_found, "total error" + mt.plur(err_found))
+else:
+    print("SUCCESS! The mistake file and test file match.")
