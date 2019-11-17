497e5892180050fb214ca36adc56fc1b9fce580a
andrewschultz <schultz.andrew@sbcglobal.net>
2019-11-16 18:23:15 -0600

    broke homonym checking to by-file function with array of tables in hgc

--- hgc.py
+++ hgc.py
@@ -26,6 +26,41 @@ def usage(msg = "General usage"):
 
 cmd_count = 1
 
+def comb_one_file(file_name, tables_needed, cols_needed = [0])
+    tables_got = defaultdict(bool)
+    for x in tables_needed: tables_got[x] = False
+    bn = os.path.basename(file_name)
+    with open(file_name) as file:
+        for (line_count, line) in enumerate(file, 1):
+            if line.startswith("table of"):
+                temp_table = re.sub(" *\[.*", "", line.strip())
+                if temp_table in tables_got:
+                    if tables_got[temp_table]:
+                        print("WARNING duplicate table start", temp_table, bn, line_count
+                    cur_table = temp_table
+                    cur_table_start = line_count + 1
+                    tables_got[temp_table] = True
+                continue
+            if not line.strip():
+                cur_table = ""
+            if not cur_table: continue
+            if line_count == cur_table_start: continue # get rid of header row
+            ary = line.split("\t")
+            for col in cols_needed:
+                hom_cand = ' '.join(ary[0].split('"')[1::2])
+                a2 = hom_cand.split(" ")
+                for q in a2:
+                    if q in hom.hom_list:
+                        if q in hom_ignore: continue
+                        if q in already_got and ignore_already_got: continue
+                        count += 1
+                        print("{}: {}{} <{}> line {} in <{}> may need homonyms: {}.".format(count, "(already got) " if q in already_got else "", q, bn, line_count, cur_table, ", ".join(hom.hom_list[q])))
+                        already_got[q] = line_count
+                        mytools.add_postopen_file_line(file_name, line_count)
+    print("Summary of stuff to replace:", ', '.join(sorted(already_got)))
+    for x in tables_got:
+        if not tables_got[x]: print("Did not find", x, "in", bn)
+
 while cmd_count < len(sys.argv):
     arg = nohy(sys.argv[1:])
     if arg == 'ia': ignore_already_got = True
@@ -34,29 +69,7 @@ while cmd_count < len(sys.argv):
     else: usage("Bad argument {}".format(arg))
     cmd_count += 1
 
-with open(vvm) as file:
-    bn = os.path.basename(vvm)
-    for (line_count, line) in enumerate(file, 1):
-        if line.startswith("table of"):
-            cur_table = re.sub(" *\[.*", "", line.strip())
-            cur_table_start = line_count + 1
-            continue
-        if not line.strip():
-            cur_table = ""
-        if not cur_table: continue
-        if line_count == cur_table_start: continue # get rid of header row
-        ary = line.split("\t")
-        hom_cand = ' '.join(ary[0].split('"')[1::2])
-        a2 = hom_cand.split(" ")
-        for q in a2:
-            if q in hom.hom_list:
-                if q in hom_ignore: continue
-                if q in already_got and ignore_already_got: continue
-                count += 1
-                print("{}: {}{} <{}> line {} in <{}> may need homonyms: {}.".format(count, "(already got) " if q in already_got else "", q, bn, line_count, cur_table, ", ".join(hom.hom_list[q])))
-                already_got[q] = line_count
-                mytools.add_postopen_file_line(vvm, line_count)
-
-print("Summary of stuff to replace:", ', '.join(sorted(already_got)))
+comb_one_file(src, ["table of verb checks"], [0, 1])
+comb_one_file(vvm, ["table of mistake substitutions", "table of homonym rejections"], [0])
 
 mytools.postopen_files()
