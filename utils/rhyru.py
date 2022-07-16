b5f3cabee0bac1934a3c8afea67d26ad21f666d1
Andrew <blurglecruncheon@gmail.com>
2022-07-15 20:07:14 -0500

    rhyru.py refined, then tested on VVFF to make sure no false positives

--- utils/rhyru.py
+++ utils/rhyru.py
@@ -2,30 +2,40 @@
 # rhyru.py: checks vc- rhyming rules in the local CSDD table file
 #
 
+import re
 import i7
 
-next_needed = prev_cue = ""
+def anything_but(my_ary, my_text):
+    for x in my_ary:
+        if x != my_text:
+            return True
+    return False
 
-this_proj = i7.proj2dir()
+this_proj = i7.dir2proj()
 my_file = i7.hdr(this_proj, 'ta')
+
+addables = [ 'vcp', 'vcal', 'say' ]
+my_ary = []
 
 with open(my_file) as file:
     for (line_count, line) in enumerate(file, 1):
-        if 'vcal' in line:
-            if prev_cue and prev_cue != 'vcal': print("Cue crossup line", line_count)
-            prev_cue = 'vcal'
-            next_needed = "already-done";
-            continue
-        if 'vcp' in line:
-            if prev_cue and prev_cue != 'vcp': print("Cue crossup line", line_count)
-            prev_cue = 'vcp'
-            next_needed = "not-yet";
-            continue
-        if next_needed:
-            if "\telse" in line: continue
-            if next_needed not in line:
-                print("Line", line_count," needs", next_needed, "after", prev_cue)
-                continue
-            next_needed = ""
-            prev_cue = ""
-      t      continue
+        l0 = line.strip()
+        if "this is the vc-" in l0:
+            this_rule = re.sub("^.*?\(", "", line.strip())
+            this_rule = re.sub("\).*", "", this_rule)
+            my_ary = []
+        for x in addables:
+            if l0.startswith(x) and '[oksay]' not in line:
+                my_ary.append(x)
+        if 'already-done;' in line:
+            if anything_but(my_ary, 'vcal'):
+                print(this_rule, "already-done has non-vcal print above line", line_count)
+            my_ary = []
+        if 'not-yet;' in line:
+            if anything_but(my_ary, 'vcp'):
+                print(this_rule, "not-yet has non-vcp print above line", line_count)
+            my_ary = []
+        if 'ready;' in line:
+            if anything_but(my_ary, 'xxx'):
+                print(this_rule, "ready has printing above line", line_count)
+            my_ary = []
